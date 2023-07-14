819edb5e7e82dd98cb297c21ff484d3e35904805
OzgeOzyurt <ozgeozyurt00@gmail.com>
2023-07-14 22:33:55 +0300

    Tüm Modeller içerisine Display bilgisi eklendi.

--- NETCoreMVCBlog/Models/Kullanici.cs
+++ NETCoreMVCBlog/Models/Kullanici.cs
@@ -1,10 +1,16 @@
-﻿namespace NETCoreMVCBlog.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace NETCoreMVCBlog.Models
 {
     public class Kullanici
     {
-        public int Id { get; set; } 
+        public int Id { get; set; }
+        [Display(Name = "Kullanıcı Adı")] 
         public string Ad { get; set; }
+        [Display(Name = "Kullanıcı Soyadı")] 
         public string Soyad { get; set; }
+        [Display(Name = "Kullanıcı Takma Adı")] 
         public string Nickname{ get; set; }
         public bool Aktif { get; set; }
     }
